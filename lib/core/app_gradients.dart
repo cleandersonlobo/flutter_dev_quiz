69aada32ed75e5d171b2dfb229ab212becfa24a1
Cleanderson Lobo <cleandersonlobo@gmail.com>
2021-04-20 01:02:41 -0300

    feat: update gradients color

--- lib/core/app_gradients.dart
+++ lib/core/app_gradients.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 class AppGradients {
   static final linear = LinearGradient(colors: [
     Color(0xFF57B6E5),
-    Color.fromRGBO(130, 87, 229, 0.695),
+    Color.fromRGBO(60, 40, 229, 0.695),
   ], stops: [
     0.0,
     0.695
