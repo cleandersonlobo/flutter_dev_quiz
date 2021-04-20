d02c67a69635a99d4ce559f6a6cd73a84cfa2f4d
Cleanderson Lobo <cleandersonlobo@gmail.com>
2021-04-20 00:15:35 -0300

    feat: added splash page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dev_quiz/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dev.Quiz_',
+      title: "DevQuiz",
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
     );
   }
 }
