d3dc01f73416c345100981164e3b16b1b11c962f
Cleanderson Lobo <cleandersonlobo@gmail.com>
2021-04-21 20:05:39 -0300

    feat: updated question model

--- lib/shared/models/question_model.dart
+++ lib/shared/models/question_model.dart
@@ -2,10 +2,12 @@ import 'package:flutter_dev_quiz/shared/models/awnser_mdoel.dart';
 
 class QuestionModel {
   final String title;
+  final bool isAnswered;
   final List<AwnserModel> awnsers;
 
   QuestionModel({
     required this.title,
     required this.awnsers,
+    this.isAnswered = false,
   }) : assert(awnsers.length == 4); // para garantir o tamanho do layout
 }
