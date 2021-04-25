849b418f346e03ca067f00b09bf5d0b0b151f14d
Cleanderson Lobo <cleandersonlobo@gmail.com>
2021-04-25 13:20:17 -0300

    feat: added loading animation in the button widget

--- lib/shared/widgets/button/button_widget.dart
+++ lib/shared/widgets/button/button_widget.dart
@@ -7,13 +7,15 @@ class ButtonWidget extends StatelessWidget {
   final String label;
   final String variant;
   final double maxWidth;
-  ButtonWidget(
-      {Key? key,
-      required this.label,
-      required this.onPress,
-      required this.variant,
-      this.maxWidth = 164})
-      : assert(['primary', "secondary", "light", "dark"].contains(variant)),
+  final bool isLoading;
+  ButtonWidget({
+    Key? key,
+    required this.label,
+    required this.onPress,
+    required this.variant,
+    this.maxWidth = 164,
+    this.isLoading = false,
+  })  : assert(['primary', "secondary", "light", "dark"].contains(variant)),
         super(key: key);
 
   ButtonWidget.primary(
@@ -23,6 +25,7 @@ class ButtonWidget extends StatelessWidget {
       : this.variant = 'primary',
         this.onPress = onPress,
         this.label = label,
+        this.isLoading = false,
         this.maxWidth = maxWidth;
 
   ButtonWidget.light(
@@ -32,6 +35,7 @@ class ButtonWidget extends StatelessWidget {
       : this.variant = 'light',
         this.onPress = onPress,
         this.label = label,
+        this.isLoading = false,
         this.maxWidth = maxWidth;
 
   ButtonWidget.secondary({
@@ -41,6 +45,7 @@ class ButtonWidget extends StatelessWidget {
   })  : this.variant = 'secondary',
         this.onPress = onPress,
         this.label = label,
+        this.isLoading = false,
         this.maxWidth = maxWidth;
 
   final buttonConfig = {
@@ -65,37 +70,59 @@ class ButtonWidget extends StatelessWidget {
   Color get buttonColor => buttonConfig[variant]!['button']!;
   Color get fontColor => buttonConfig[variant]!['text']!;
 
+  _buttonWidget() {
+    return Container(
+      width: double.infinity,
+      constraints: BoxConstraints(maxWidth: maxWidth),
+      child: Ink(
+        decoration: BoxDecoration(
+            color: buttonColor,
+            border: variant != 'light'
+                ? null
+                : Border.fromBorderSide(
+                    BorderSide(color: AppColors.border, width: 1)),
+            borderRadius: BorderRadius.circular(10)),
+        child: InkWell(
+          onTap: onPress,
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 14),
+              child: Text(label,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.notoSans(
+                    fontWeight: FontWeight.w600,
+                    color: fontColor,
+                    fontSize: 13,
+                  )),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _isLoadingWidget() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(buttonColor),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       type: MaterialType.transparency,
-      child: Container(
-        width: double.infinity,
-        constraints: BoxConstraints(maxWidth: maxWidth),
-        child: Ink(
-          decoration: BoxDecoration(
-              color: buttonColor,
-              border: variant != 'light'
-                  ? null
-                  : Border.fromBorderSide(
-                      BorderSide(color: AppColors.border, width: 1)),
-              borderRadius: BorderRadius.circular(10)),
-          child: InkWell(
-            onTap: onPress,
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 14),
-                child: Text(label,
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.notoSans(
-                      fontWeight: FontWeight.w600,
-                      color: fontColor,
-                      fontSize: 13,
-                    )),
-              ),
-            ),
-          ),
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 100),
+        transitionBuilder: (Widget child, Animation<double> animation) {
+          return FadeTransition(child: child, opacity: animation);
+        },
+        child: Container(
+          key: ValueKey<bool>(isLoading),
+          child: isLoading ? _isLoadingWidget() : _buttonWidget(),
         ),
       ),
     );
